e469c99e4713134923b7a35b1bae5f0ff7dc8950
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-11 12:46:52 -0400

    bouncing off paddles

--- pong/main.py
+++ pong/main.py
@@ -15,7 +15,7 @@ YELLOW = 255, 255, 96
 BLACK = 0, 0, 0
 
 left_paddle = pygame.Rect(10, 200, 10, 75)
-right_paddle = pygame.Rect(780, 200, 10, 75)
+right_paddle = pygame.Rect(780, 0, 10, 600)
 ball_center = 40, 50
 BALL_RADIUS = 9
 
@@ -23,13 +23,25 @@ pygame.draw.rect(DISPLAYSURF, RED, left_paddle)
 pygame.draw.rect(DISPLAYSURF, BLUE, right_paddle)
 pygame.draw.circle(DISPLAYSURF, YELLOW, ball_center, BALL_RADIUS)
 
-ball_x_delta = 2
-ball_y_delta = 1
-while True:
-    if ball_center[0] <= BALL_RADIUS or ball_center[0] >= WIDTH - BALL_RADIUS:
-        ball_x_delta = -ball_x_delta
+ball_x_delta = 5
+ball_y_delta = 2
+
+
+def touch_top(ball_center):
     if ball_center[1] <= BALL_RADIUS or ball_center[1] >= HEIGHT - BALL_RADIUS:
+        return True
+
+
+def touch_paddle(ball_center, left_paddle, right_paddle):
+    ball_rect = ball_center[0] - BALL_RADIUS, ball_center[1] - BALL_RADIUS, 2 * BALL_RADIUS, 2 * BALL_RADIUS
+    return left_paddle.colliderect(ball_rect) or right_paddle.colliderect(ball_rect)
+
+while True:
+    if touch_top(ball_center):
         ball_y_delta = -ball_y_delta
+
+    if touch_paddle(ball_center, left_paddle, right_paddle):
+        ball_x_delta = -ball_x_delta
 
     new_ball_center = ball_center[0] + ball_x_delta, ball_center[1] + ball_y_delta
     pygame.draw.circle(DISPLAYSURF, BLACK, ball_center, BALL_RADIUS)
@@ -41,4 +53,4 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-    fps_clock.tick(150)
+    fps_clock.tick(50)
