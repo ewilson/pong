8d1c53e5d3af9dd4b4cd51fd80207506905d7c41
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-11 19:24:56 -0400

    two moving paddles

--- pong/main.py
+++ pong/main.py
@@ -15,7 +15,7 @@ YELLOW = 255, 255, 96
 BLACK = 0, 0, 0
 
 left_paddle = pygame.Rect(10, 200, 10, 75)
-right_paddle = pygame.Rect(780, 0, 10, 600)
+right_paddle = pygame.Rect(780, 0, 10, 75)
 ball_center = 40, 50
 BALL_RADIUS = 9
 
@@ -27,20 +27,21 @@ ball_x_delta = 7
 ball_y_delta = 4
 TOC = 50
 
-def touch_top(ball_center):
+
+def touch_top():
     if ball_center[1] <= BALL_RADIUS or ball_center[1] >= HEIGHT - BALL_RADIUS:
         return True
 
 
-def touch_paddle(ball_center, left_paddle, right_paddle):
+def touch_paddle():
     ball_rect = ball_center[0] - BALL_RADIUS, ball_center[1] - BALL_RADIUS, 2 * BALL_RADIUS, 2 * BALL_RADIUS
     return left_paddle.colliderect(ball_rect) or right_paddle.colliderect(ball_rect)
 
 while True:
-    if touch_top(ball_center):
+    if touch_top():
         ball_y_delta = -ball_y_delta
 
-    if touch_paddle(ball_center, left_paddle, right_paddle):
+    if touch_paddle():
         ball_x_delta = -ball_x_delta
 
     new_ball_center = ball_center[0] + ball_x_delta, ball_center[1] + ball_y_delta
@@ -50,16 +51,25 @@ while True:
 
     pygame.event.pump()
     keys = pygame.key.get_pressed()
+    left_delta = 0
     if keys[K_a]:
-        new_lp = left_paddle.move(0, -5)
-        pygame.draw.rect(DISPLAYSURF, BLACK, left_paddle)
-        pygame.draw.rect(DISPLAYSURF, RED, new_lp)
-        left_paddle = new_lp
-    elif keys[K_z]:
-        new_lp = left_paddle.move(0, 5)
-        pygame.draw.rect(DISPLAYSURF, BLACK, left_paddle)
-        pygame.draw.rect(DISPLAYSURF, RED, new_lp)
-        left_paddle = new_lp
+        left_delta = -10
+    if keys[K_z]:
+        left_delta = 10
+    new_lp = left_paddle.move(0, left_delta)
+    pygame.draw.rect(DISPLAYSURF, BLACK, left_paddle)
+    pygame.draw.rect(DISPLAYSURF, RED, new_lp)
+    left_paddle = new_lp
+
+    right_delta = 0
+    if keys[K_UP]:
+        right_delta = -10
+    if keys[K_DOWN]:
+        right_delta = 10
+    new_rp = right_paddle.move(0, right_delta)
+    pygame.draw.rect(DISPLAYSURF, BLACK, right_paddle)
+    pygame.draw.rect(DISPLAYSURF, BLUE, new_rp)
+    right_paddle = new_rp
 
     for event in pygame.event.get():
         if event.type == QUIT:
