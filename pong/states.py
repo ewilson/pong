4c170ab1e04dc8516e25a5b83679ccfa95ea9b05
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-14 05:26:49 -0400

    completed welcome screen

--- pong/states.py
+++ pong/states.py
@@ -1,6 +1,7 @@
+import pygame
 from pygame.locals import *
 
-from board import Board
+from board import Board, DISPLAYSURF, HEIGHT, WIDTH, BLACK
 
 
 class Play(object):
@@ -28,14 +29,32 @@ class Welcome(object):
     def __init__(self):
         self.quit = False
         self.done = False
+        self.ready = False
         self.next = 'play'
+        self.font_size = 1
+        self.normal_font = pygame.font.SysFont("monospace", 15)
 
     def update(self, keys):
+        if self.font_size <= 200:
+            welcome = pygame.font.SysFont("monospace", self.font_size)
+            pong = welcome.render("PONG!", 1, (0,255,0))
+            w = pong.get_width()
+            h = pong.get_height()
+            x = (WIDTH - w) / 2
+            y = (HEIGHT - h) / 2
+            DISPLAYSURF.fill(BLACK, (x, y, w, h))
+            DISPLAYSURF.blit(pong, (x,y))
+            self.font_size += 1
+        elif not self.ready:
+            begin = self.normal_font.render("Press space bar to start", 1, (0,255,0))
+            DISPLAYSURF.blit(begin, ((WIDTH - begin.get_width()) / 2, 400))
+            self.ready = True
         if keys[K_SPACE]:
             self.done = True
 
     def cleanup(self):
         self.done = False
+        DISPLAYSURF.fill(BLACK)
 
 
 class GameOver(object):
