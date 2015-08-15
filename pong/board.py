1c1d09f91cef0df84788536962058261e2321306
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-15 16:36:35 -0400

    using image for ball

--- pong/board.py
+++ pong/board.py
@@ -44,35 +44,45 @@ class Paddle(object):
 
 class Ball(object):
 
-    def __init__(self, center, radius, color, delta):
+    def __init__(self, center, diameter, delta):
         self.center = center
-        self.radius = radius
-        self.color = color
+        self.upper_left = center[0] - diameter / 2, center[1] - diameter / 2
+        self.diameter = diameter
         self.delta = delta
         self.click = pygame.mixer.Sound("../paddle.wav")
         self.bounce = pygame.mixer.Sound("../bounce.wav")
+        orig_img = pygame.image.load("../tennis_ball.png")
+        self.img = pygame.transform.scale(orig_img, (diameter, diameter))
+        self.rect = pygame.Rect(self.upper_left[0], self.upper_left[1],
+                     self.diameter, self.diameter)
+
+    def reset(self, center):
+        self.upper_left = center[0] - self.diameter / 2, center[1] - self.diameter / 2
+        self.rect = pygame.Rect(self.upper_left[0], self.upper_left[1],
+                                self.diameter, self.diameter)
 
     def draw(self):
-        pygame.draw.circle(DISPLAYSURF, self.color, self.center, self.radius)
+        DISPLAYSURF.blit(self.img, self.upper_left)
+        pygame.display.flip()
 
     def move(self):
-        pygame.draw.circle(DISPLAYSURF, BLACK, self.center, self.radius)
-        self.center = self.center[0] + self.delta[0], self.center[1] + self.delta[1]
-        pygame.draw.circle(DISPLAYSURF, self.color, self.center, self.radius)
+        pygame.draw.rect(DISPLAYSURF, BLACK, self.rect)
+        self.rect = self.rect.move(*self.delta)
+        DISPLAYSURF.blit(self.img, self.rect.topleft)
+        pygame.display.flip()
 
     def touch_top(self):
-        return self.center[1] <= self.radius or self.center[1] >= HEIGHT - self.radius
+        return self.rect.top <= 0 or self.rect.bottom >= HEIGHT
 
     def touch_paddle(self, left_p, right_p):
-        rect = self._rect()
-        return left_p.rect.colliderect(rect) or right_p.rect.colliderect(rect)
+        return self.rect.colliderect(left_p.rect) or self.rect.colliderect(right_p.rect)
 
     def bounce_wall(self):
         self.delta = (self.delta[0], -self.delta[1])
         self.bounce.play()
 
     def bounce_paddle(self, left_p, right_p):
-        rect = self._rect()
+        rect = self.rect
         if rect.left < left_p.rect.left or rect.right > right_p.rect.right:
             self.delta = (self.delta[0], -self.delta[1])
         else:
@@ -80,16 +90,10 @@ class Ball(object):
         self.click.play()
 
     def escape_right(self):
-        return self.center[0] - self.radius > WIDTH
+        return self.rect.left > WIDTH
 
     def escape_left(self):
-        return self.center[0] + self.radius < 0
-
-    def _rect(self):
-        half_x = self.radius + abs(self.delta[0])
-        half_y = self.radius + abs(self.delta[1])
-        return pygame.Rect(self.center[0] - half_x, self.center[1] - half_y,
-                2 * half_x, 2 * half_y)
+        return self.rect.right < 0
 
 
 class Board(object):
@@ -97,7 +101,7 @@ class Board(object):
     def __init__(self):
         self.left_paddle = Paddle(pygame.Rect(10, 200, P_WIDTH, P_HEIGHT), RED)
         self.right_paddle = Paddle(pygame.Rect(780, 200, P_WIDTH, P_HEIGHT), BLUE)
-        self.ball = Ball(BALL_START, 9, YELLOW, (7, 4))
+        self.ball = Ball(BALL_START, 40, (7, 4))
         self.left_score = 0
         self.right_score = 0
         self.left_paddle.draw()
@@ -119,12 +123,12 @@ class Board(object):
             self.left_score += 1
             self._print_score()
             self.ball.delta = -self.ball.delta[0], self.ball.delta[1]
-            self.ball.center = BALL_START
+            self.ball.reset(BALL_START)
         elif self.ball.escape_left():
             self.right_score += 1
             self._print_score()
             self.ball.delta = -self.ball.delta[0], self.ball.delta[1]
-            self.ball.center = BALL_START
+            self.ball.reset(BALL_START)
 
         if keys[K_a]:
             self.left_paddle.move(-1)
